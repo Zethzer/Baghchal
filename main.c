4a4b038339abc9be8c3bbf74cc61b7843820fabf
Plopounet13 <lois@MacBook-Air-de-Lois.local>
2014-05-11 12:25:18 +0200

    main test

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 
 Plateau plat;
 
+extern WINDOW* winHist;
+
 /*void init(char* nomJoueurC, char* nomJoueurT){
     if(affichage_menu())
         affichage_initJoueurs(nomJoueurC, nomJoueurT);
@@ -20,66 +22,27 @@ Plateau plat;
 }*/
                              
 int main(void){
-<<<<<<< HEAD
     int ch;
-    Mvt m;
     Historique h;
-    Coup* c;
-    Pos p;
-    p.x =2;
-    p.y =3;
-=======
-    int ch;// codeVainqueur, codeTour;
-    Historique h;
-    // MEVENT event;
-    // Pos p;
-    // p.x =2;
-    // p.y =3;
->>>>>>> FETCH_HEAD
+    Pos p = gestionPions_initPos (2, 3);
+    Mvt m = gestionPions_initMvt (1, 1, 0, 0);
+    Coup* c = historique_init_coup (m, 1, 0, 0 ,0);
     
-    //initialisation plateau
-    plateau_init();
-    
-    //initialisation Ncurses
-    initscr();
-    keypad(stdscr, TRUE);
-    
-    //initialisation affichage
     affichage_init();
+    historique_init(&h);
     
-    /*modification Plateau
+    //Modif plateau
     plateau_ajouterChevre(p);
-<<<<<<< HEAD
     affichage_maj_plateau_case(p);
     
     //Modif Historique
-    m = gestionPions_initMvt(1, 1, 0, 0);
-    c = historique_init_coup (m, 1, 0, 0, 0);
     for (int i=0; i<22; ++i)
         historique_ajouter_coup (&h, c);
     for (int i=0; i<22; ++i)
         affichage_maj_Hist_etape (c, i);
     wrefresh(winHist);
-=======
-    affichage_maj_plateau_case(p);*/
-
-    historique_init(&h);
-
-    /*while((codeVainqueur=rechercheVainqueur_vainqueurPresent()) == 0){
-        if((codeTour=plateau_gestionTour(h)) == 0)
-            ;
-        else if (codeTour == 1){
-            plateau_init();
-            historique_init(h);
-            affichage_init();
-        }
-        else if (codeTour == 2)
-            ;
-            //save_import(affichage_boiteDialogue(1));
-    }*/
->>>>>>> FETCH_HEAD
     
-    //attente
+    //Attente
     while((ch = getch()) != KEY_F(3)){
         
     }
