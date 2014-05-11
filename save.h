7d44550f6a63a88e2c53e08a7b376b83b6c0a06a
Plopounet13 <lois@MacBook-Air-de-Lois.local>
2014-05-11 22:08:54 +0200

    commentaires finis

--- save.h
+++ save.h
@@ -1,8 +1,18 @@
 #ifndef __SAVE_H__
 #define __SAVE_H__
 
+/*
+ E : une chaine de caractère
+ S : aucune
+ Role : écrit dans un fichier nommé nomSave situé dans le dossier /save les informations sur l'état de la partie. 
+ */
 void save_export (char* nomSave);
 
+/*
+ E : une chaine de caractère
+ S : aucune
+ Role : charge les informations propres à la partie depuis un fichier nommé nomSave situé dans le dossier /save
+ */
 void save_import (char* nomSave);
 
 #endif
