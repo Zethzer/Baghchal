f90614efe7242f24dcebfb1d49a053940141c5a4
Plopounet13 <lois@MacBook-Air-de-Lois.local>
2014-05-11 22:05:08 +0200

    fin des commentaires

--- affichage.h
+++ affichage.h
@@ -138,22 +138,66 @@ void affichage_maj_plateau_case (Pos p);
  */
 void affichage_maj_plateau (void);
 
+/*
+ E : aucune
+ S : aucune
+ Role : Met à jour la fenetre affichant le nombre de chevres restantes à placer
+ */
 void affichage_maj_rest (void);
 
+/*
+ E : aucune
+ S : aucune
+ Role : Met à jour la fenetre affichant le nombre de chevres capturees
+ */
 void affichage_maj_capture (void);
 
+/*
+ E : un pointeur vers un Coup et un numéro de ligne
+ S : aucune
+ Role : affiche les informations correspondantes à un coup à la ligne ligne dans la fenetre d'historique
+*/
 void affichage_maj_Hist_etape (Coup* c, int ligne);
 
+/*
+ E : L'historique
+ S : aucune
+ Role : affiche les informations correspondantes à tous les coups de l'historique dans la mesure de la fenetre historique.
+ */
 void affichage_maj_Hist (Historique h);
 
+/*
+ E : Historique
+ S : aucune
+ Role : Met à jour toutes les fenêtres dont le contenu est modifiable.
+ */
 void affichage_maj_all (Historique h);
 
+/*
+ E : un pointeur vers l'historique
+ S : aucune
+ Role : Décale vers le haut la liste des coups joues
+ */
 void affichage_scrollup_hist (Historique* h);
 
+/*
+ E : un pointeur vers l'historique
+ S : aucune
+ Role : Décale vers le bas la liste des coups joues
+ */
 void affichage_scrolldown_hist (Historique* h);
 
+/*
+ E : i un booleen
+ S : une chaine de caractere
+ Role : i gère l'affichage du message dans la fenetre de Chat, et l'utilisateur saisi une chaine de caractere */
 void affichage_boiteDialogue(int i, char* str);
 
+/*
+ E : tigre un booleen
+ S : aucune
+ Role : affiche une fenetre declarant le gagnant (tigre sert à savoir lequel des joueurs a gagné)
+ */
 WINDOW* affichage_message_victoire (int tigre);
 
 #endif
