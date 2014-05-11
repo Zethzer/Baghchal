02094a6d3291b42a7b763f658286f8afb8a18b6e
Plopounet13 <lois@MacBook-Air-de-Lois.local>
2014-05-11 22:29:08 +0200

    commentaires finis

--- evenement.h
+++ evenement.h
@@ -4,6 +4,15 @@
 #include "gestionPions.h"
 #include "historique.h"
 
+/*
+ E : un pointeur vers l'historique
+ S : Retourne : 
+    La position d'un clic
+    ET :
+        * 1 si il y a eu un clic
+        * 0 si il y a eu autre chose
+ Role : récupère les coordonnées d'un clic ou s'occupe de déclencher le scrolling de l'historique si UP ou DOWN est pressé.
+ */
 int evenement_recupererEvenement(Historique* h, Pos* pEvent);
 
 #endif
