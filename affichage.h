201dd14cbaa6439ab2bdabd831067b87fa33eb8a
Plopounet13 <lois@MacBook-Air-de-Lois.local>
2014-05-11 15:38:52 +0200

    aff.h

--- affichage.h
+++ affichage.h
@@ -39,6 +39,8 @@ void affichage_message (char* Message, int color);
 
 void affichage_maj_plateau_case (Pos p);
 
+void affichage_maj_plateau (void);
+
 void affichage_maj_rest (void);
 
 void affichage_maj_capture (void);
