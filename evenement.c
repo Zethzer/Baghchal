4eab59334cac5ecc84f23c4e415d76392bb5893f
Bernard Yannick <bernard.y24@gmail.com>
2014-05-11 14:14:10 +0200

    modifs evenement

--- evenement.c
+++ evenement.c
@@ -1,5 +1,6 @@
 #include "evenement.h"
 
+#include <stdbool.h>
 #include <ncurses.h>
 
 Pos evenement_recupererEvenementSouris(){
@@ -7,17 +8,19 @@ Pos evenement_recupererEvenementSouris(){
 	int c;
 	MEVENT event;
 
-	mousemask(BUTTON1_PRESSED | BUTTON1_RELEASED, NULL);
+	keypad(stdscr, true);
+	mousemask(ALL_MOUSE_EVENTS, NULL);
 
-	c=getch();
+	c=(int) getch();
 	switch(c){
 		case KEY_MOUSE:
 			if(getmouse(&event) == OK){
-				if(event.bstate && BUTTON1_PRESSED){
+				if(event.bstate & BUTTON1_PRESSED){
 					p.x=event.x;
 					p.y=event.y;
 				}
 			}
+		break;
 	}
 
 	return(p);
